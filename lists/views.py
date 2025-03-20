d1ef40027bd5c8a4d4f0527a414d56a1ce93f9e7
rafacu-dev <rafaproyectosfuturos@gmail.com>
2025-03-20 19:20:06 -0400

    ok

--- lists/views.py
+++ lists/views.py
@@ -17,26 +17,38 @@ class ListAPIView(APIView):
     
     def post(self, request, *args, **kwargs):
         serializer = ListSerializer(data=request.data)
-        print("**********************",request.data)
-
         if serializer.is_valid():
             elements_data = request.data.get('elements')
             user = request.user
             date = parse_datetime(request.data.get('date'))
+
+            if request.data.get('publishedId'):
+                list_instance = List.objects.get(id=request.data.get('publishedId'), user=user)
+                list_instance.elements.clear()                
+                list_instance.name=request.data.get('name')
+                list_instance.language=request.data.get('language')
+                list_instance.visible= True if request.data.get('visibility') != "private" else False
+                list_instance.hashtags=request.data.get('hashtags')
+                list_instance.category=request.data.get('category')
+                list_instance.date=date
+            else:
             
-            list_instance = List.objects.create(
-                                                user=user, 
-                                                name=request.data.get('name'), 
-                                                language=request.data.get('language'), 
-                                                visible= True if request.data.get('visibility') != "private" else False, 
-                                                hashtags=request.data.get('hashtags'), 
-                                                category=request.data.get('category'),
-                                                date=date
-                                            )
+                list_instance = List.objects.create(
+                                                    user=user, 
+                                                    name=request.data.get('name'), 
+                                                    language=request.data.get('language'), 
+                                                    visible= True if request.data.get('visibility') != "private" else False, 
+                                                    hashtags=request.data.get('hashtags'), 
+                                                    category=request.data.get('category'),
+                                                    date=date
+                                                )
+
             for element_data in elements_data:
                 element= Element.objects.create(text=element_data.get("text"))
                 list_instance.elements.add(element)
 
             list_instance.save()
+
+
             return Response(list_instance.id, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
