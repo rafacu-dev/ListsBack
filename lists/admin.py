bb3841ff94f6cd0ba8389a9083d1abacb3a360d6
rafacu-dev <rafaproyectosfuturos@gmail.com>
2025-03-20 21:19:25 -0400

    ok

--- lists/admin.py
+++ lists/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from lists.models import Element, List, Inspired
+
+admin.site.register(Element)
+admin.site.register(List)
+admin.site.register(Inspired)
